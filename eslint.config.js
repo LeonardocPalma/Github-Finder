5b47385b6c99d8cc83f2196a7850399a081faf11
Leonardo Palma <leonardoc.palma@outlook.com>
2025-04-22 21:51:08 -0300

    feat:creating routes and component of Search

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import js from "@eslint/js";
 import globals from "globals";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
+
 import reactRefresh from "eslint-plugin-react-refresh";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -23,6 +24,7 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css'
+import "./App.css";
+
+import { Outlet } from "react-router-dom";
 
 function App() {
-
-
   return (
     <div className="app">
       <h1>GitHub Finder</h1>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
