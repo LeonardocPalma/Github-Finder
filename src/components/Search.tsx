baf60bb5f7d3689b227ee9d503eee27e1b0f0b79
Leonardo Palma <leonardoc.palma@outlook.com>
2025-04-23 21:38:08 -0300

    feat:coding css module from search anda app

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { KeyboardEvent, useState } from "react";
 import { BsSearch } from "react-icons/bs";
+
+import classes from "./Search.module.css";
 
 type SearchProps = {
   loadUser: (userName: string) => Promise<void>;
@@ -9,15 +11,22 @@ type SearchProps = {
 const Search = ({ loadUser }: SearchProps) => {
   const [userName, setUserName] = useState("");
 
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      loadUser(userName);
+    }
+  };
+
   return (
-    <div>
+    <div className={classes.search}>
       <h2>Busque por um usuário:</h2>
       <p>Conheça seus melhores repositórios</p>
-      <div>
+      <div className={classes.search_container}>
         <input
           type="text"
           placeholder="Digite o nome do usuário"
           onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
         <button onClick={() => loadUser(userName)}>
           <BsSearch />
