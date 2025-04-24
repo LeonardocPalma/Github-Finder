85ef6527494437f197be606b394842d97b41d63f
Leonardo Palma <leonardoc.palma@outlook.com>
2025-04-23 21:47:36 -0300

    feat:coding component of error

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import Error from "../components/Error";
 import Search from "../components/Search";
 import User from "../components/User";
 import { UserProps } from "../types/user";
@@ -7,9 +8,18 @@ import { UserProps } from "../types/user";
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
 
+  const [error, setError] = useState(false);
+
   const loadUser = async (userName: string) => {
+    setError(false);
+    setUser(null);
     const res = await fetch(`https://api.github.com/users/${userName}`);
     const data = await res.json();
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
 
     const { avatar_url, login, location, followers, following } = data;
 
@@ -28,6 +38,7 @@ const Home = () => {
     <div>
       <Search loadUser={loadUser} />
       {user && <User {...user} />}
+      {error && <Error />}
     </div>
   );
 };
